b76d35034de7f44c50093ce5a6fa023352a91565
denis.tsapenko <denis.tsapenko90@yandex.ru>
2022-08-02 19:51:57 +0300

    Решение задачи

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,4 +4,18 @@
 //7 -> да
 //1 -> нет
 
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+if(number < 6)
+     {
+        Console.Write("Рабочий день");
+     }
+     else if(number == 6)
+     {
+        Console.Write("Суббота - выходной день");
+     }
+     else if(number == 7)
+     {
+        Console.Write("Воскресенье - выходной день");
+     }
